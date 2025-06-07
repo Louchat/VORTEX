bf814cf0c640d1e148ea9a60f91548e7596e6cef
Louchat <119514120+Louchat@users.noreply.github.com>
2025-06-07 12:11:45 +0200

    Update VORTEX v9.4.py
    VORTEX know if he is up to date or not and display the current version with the latest

--- VORTEX.py
+++ VORTEX.py
@@ -1,3 +1,9 @@
+# ==============================================================
+# VORTEX - Custom Launcher Utility
+# Developed by Louchatfluff
+# Open-source on GitHub: https://github.com/Louchat/VORTEX
+# ==============================================================
+
 import ctypes
 import time
 import shutil
@@ -13,7 +19,7 @@ import getpass
 import requests
 from colorama import Fore, init
 
-# Initialisation de colorama
+# Initialize colorama
 init(autoreset=True)
 
 
@@ -31,7 +37,7 @@ if not is_admin():
     sys.exit()
 
 
-# Demande le nom d'utilisateur de Windows
+# Ask for the Windows username
 os.system('title [LOCKED]:')
 useer = os.getlogin()
 def verify_user():
@@ -48,14 +54,14 @@ def verify_user():
 
     if choice == "1":
         print("Unlocking automatically...")
-        time.sleep(1)  # Tu peux le retirer ou le garder comme effet visuel
+        time.sleep(1)  # Optional visual delay
         return user
     else:
         manual_user = input("Enter your username manually: ").strip()
         print(f"Manual unlock for user: {manual_user}")
         return manual_user
 
-# Exemple d'utilisation :
+# Example usage :
 current_user = verify_user()
 
 
@@ -64,6 +70,17 @@ os.system('title [UNLOCKED]                                                     
 
 def clear_console():
     os.system('cls' if os.name == 'nt' else 'clear')
+
+def get_github_version():
+    try:
+        url = "https://raw.githubusercontent.com/Louchat/VORTEX/main/version.txt"
+        response = requests.get(url)
+        response.raise_for_status()
+        return response.text.strip()
+    except Exception as e:
+        print(Fore.RED + f"Error fetching GitHub version: {e}")
+        return None
+
 
 def get_latest_commit_date():
     # GitHub API to get the latest commit on the VORTEX.py file
@@ -86,15 +103,15 @@ def get_latest_commit_date():
 def update_vortex():
     repo_url = "https://raw.githubusercontent.com/Louchat/VORTEX/main/VORTEX.py"
     
-    # Récupérer le dossier où est le script courant (VORTEX.py)
+    # Get the current script's directory (VORTEX.py)
     current_dir = os.path.dirname(os.path.abspath(__file__))
     
-    # Créer le dossier Update_Vortex dans ce dossier
+    # Create Update_Vortex folder if not exist
     update_folder = os.path.join(current_dir, "Update_Vortex")
     if not os.path.exists(update_folder):
         os.makedirs(update_folder)
     
-    # Chemin complet du fichier à écrire dans Update_Vortex
+    # Full path of the downloaded update file
     local_file = os.path.join(update_folder, "VORTEX.py")
     
     choice = input("Do you want to install the latest version of VORTEX? (y/n): ").strip().lower()
@@ -116,15 +133,27 @@ def update_vortex():
         print(f"Error during update: {e}")
 
 def show_version_and_update():
-    version = "9.3"  # Your current version, update as needed
-    last_update = get_latest_commit_date()
+    version = "9.4"  # Local version
     print(f"Current VORTEX version: {version}")
+
+    
+
+    last_update = get_latest_commit_date()
     if last_update:
-        print(f"Last update on GitHub: {last_update}")
+        print(f"Last GitHub update date: {last_update}")
     else:
         print("Unable to retrieve last update date.")
-
+    github_version = get_github_version()
+    if github_version:
+        print(f"Latest version on GitHub: {github_version}")
+        if github_version != version:
+            print(Fore.YELLOW + "An update is available!")
+        else:
+            print(Fore.GREEN + "You are up to date.")
+    else:
+        print(Fore.RED + "Could not check for the latest version.")
     update_vortex()
+
 
 
 def execute_command():
@@ -163,13 +192,13 @@ def execute_command():
 
 def disconnect_second_screen():
     print(Fore.YELLOW + "Disconnecting second screen...")
-    subprocess.run(['DisplaySwitch.exe', '/external'])  # Cela bascule l'affichage sur un seul écran (l'écran principal)
+    subprocess.run(['DisplaySwitch.exe', '/external'])  # switch to single screen (to optimise graphics)
     print(Fore.GREEN + "Second screen disconnected.")
 
 
 def reconnect_second_screen():
     print(Fore.YELLOW + "Reconnecting second screen...")
-    subprocess.run(['DisplaySwitch.exe', '/extend'])  # Basculer en mode étendu (pour utiliser les deux écrans)
+    subprocess.run(['DisplaySwitch.exe', '/extend'])  # switch to extended (to use both screens)
     print(Fore.GREEN + "Second screen reconnected in extended mode.")
 
 def manage_second_screen():
@@ -182,23 +211,23 @@ def manage_second_screen():
         choice = input(Fore.CYAN + "Enter your choice (1-3): ")
 
         if choice == '1':
-            disconnect_second_screen()  # Fonction pour déconnecter le second écran
-            break  # Retour au menu principal après l'action
+            disconnect_second_screen()  # Fonction that disconect second screen
+            break  # Return to main menu after the action
         elif choice == '2':
-            reconnect_second_screen()  # Fonction pour reconnecter le second écran
-            break  # Retour au menu principal après l'action
+            reconnect_second_screen()  # Fonction that reconect second screen
+            break  # Return to main menu after the action
         elif choice == '3':
             print(Fore.YELLOW + "Going back to main menu...\n")
             break  # Retour au menu principal
         else:
             print(Fore.RED + "Invalid choice. Please try again.")
 
-def clean_disk():  # Nettoyage du dossier TEMP sans l'outil Disk Cleanup
-    temp_dir = os.getenv('TEMP')  # Récupère le chemin du dossier TEMP
+def clean_disk():  # Clean TEMP folder without using Disk Cleanup
+    temp_dir = os.getenv('TEMP')  # Get TEMP folder path
     print(Fore.YELLOW + f"Cleaning files in {temp_dir}...")
 
     try:
-        # Supprimer tous les fichiers et dossiers dans le dossier %TEMP%
+        # Delete all files and folders in the TEMP directory %TEMP%
         for root, dirs, files in os.walk(temp_dir, topdown=False):
             for name in files:
                 try:
@@ -215,7 +244,7 @@ def clean_disk():  # Nettoyage du dossier TEMP sans l'outil Disk Cleanup
 
         print(Fore.GREEN + "Temporary files deleted successfully.")
         
-        # Lancer la commande CMD taskkill pour fermer explorer.exe
+        # Kill explorer.exe process using taskkill
         print(Fore.YELLOW + "Killing explorer.exe process...")
         subprocess.run("taskkill /f /im explorer.exe", shell=True)
         print(Fore.GREEN + "Explorer.exe has been terminated.")
@@ -224,24 +253,24 @@ def clean_disk():  # Nettoyage du dossier TEMP sans l'outil Disk Cleanup
         print(Fore.RED + f"An error occurred while cleaning TEMP files: {e}")
 
 
-# Fonction pour régler l'opacité de la fenêtre de la console
+# Function to set console window opacity
 def set_opacity(opacity):
     hwnd = ctypes.windll.kernel32.GetConsoleWindow()
     alpha = int(255 * (opacity / 100))
     ctypes.windll.user32.SetLayeredWindowAttributes(hwnd, 0, alpha, 0x00000002)
 
-# Fonction pour cacher la fenêtre et attendre la réactivation
+# Function to hide the console and wait for reactivation
 def wait_for_reactivation():
 
     hwnd = ctypes.windll.kernel32.GetConsoleWindow()
 
-    # Cacher la fenêtre au lieu de la minimiser
+    # Hide window instead of minimizing
     win32gui.ShowWindow(hwnd, win32con.SW_HIDE)
 
     print(Fore.CYAN + "\n[!] Program is now running in the background. Press '/' to reopen the menu.")
     while True:
         if keyboard.is_pressed('/'):
-            # Restaurer la fenêtre
+            # Restore the window
             win32gui.ShowWindow(hwnd, win32con.SW_RESTORE)
             clear_console()
             print(Fore.GREEN + "Reopening launcher...")
@@ -249,7 +278,7 @@ def wait_for_reactivation():
             return
         time.sleep(0.1)
 
-# Fonction pour trouver le chemin de Discord
+# Function to find Discord executable path
 def find_discord_exe(useer):
     base_path = fr"C:\Users\{useer}\AppData\Local\Discord"
     for folder in os.listdir(base_path):
@@ -259,7 +288,7 @@ def find_discord_exe(useer):
                 return path
     return None
 
-# Fonction pour télécharger et installer une application
+# Function to download and install an application
 def install_app(app_name):
     apps = {
         "chrome": {
@@ -298,7 +327,7 @@ def install_app(app_name):
     else:
         print(Fore.RED + "Unknown app.")
 
-# Réglage de l'opacité de la fenêtre
+# Set window opacity
 set_opacity(72)
 clear_console()
 print(Fore.MAGENTA + "// DEVELOPPED BY LOUCHATFLUFF //")
@@ -306,7 +335,7 @@ time.sleep(3)
 print(Fore.RED + "THIS SOFTWARE IS FREE, IF YOU PAYED FOR IT: PLEASE RETURN IT")
 time.sleep(5)
 clear_console()
-# Menu principal
+# Main menu
 def main_menu():
     while True:
         print(Fore.GREEN + "What do u want to do:")
@@ -325,11 +354,11 @@ def main_menu():
 
         
         
-        if main_choice == '6':  # Option pour déconnecter le second écran
+        if main_choice == '6':  # Option to disconnect second screen
             manage_second_screen()
 
         elif main_choice == '1':
-            # Menu pour ouvrir des apps
+            # Menu to open apps
             while True:
                 print(Fore.YELLOW + "\nChoose an app to open:")
                 print(Fore.CYAN + "1: Riot")
@@ -376,14 +405,14 @@ def main_menu():
 
         elif main_choice == '9':
             print(Fore.RED + "Closing the program...")
-            os.system('cls')  # Vider l'écran (facultatif)
+            os.system('cls')  # Clear screen (optional)
             sys.exit()
 
         elif main_choice == "7":    
             execute_command()
 
         elif main_choice == '2':
-            # Optimisation des apps
+            # App optimization
             print(Fore.GREEN + "Optimising your system...")
             clean_disk()
             processes = ["steam.exe", "opera.exe", "sunshine.exe", "sunshinesvc.exe", "Spotify.exe", "discord.exe"]
@@ -399,12 +428,12 @@ def main_menu():
 
             print(Fore.GREEN + "Done.\n")
             print(Fore.YELLOW + "Press Enter to continue...")
-            keyboard.read_event()  # Attend une touche pour continuer, après l'optimisation
+            keyboard.read_event()  # Wait for user input before continuing
 
             clear_console()
 
         elif main_choice == '3':
-            # Menu pour installer des apps
+            # Menu to install apps
             print(Fore.YELLOW + "Choose an app to install:")
             print(Fore.CYAN + "1: Google Chrome")
             print(Fore.CYAN + "2: Opera Browser")
@@ -416,7 +445,7 @@ def main_menu():
             install_app(apps_map.get(install_choice, ''))
 
         elif main_choice == '4':
-            # Minimise le programme
+            # Minimize the program
             wait_for_reactivation()
 
 
@@ -446,11 +475,11 @@ def main_menu():
             
 
 
-        elif main_choice == '5':  # Option "Ping"
+        elif main_choice == '5':  # Ping option
             print(Fore.YELLOW + "Enter the website URL you want to visit:")
             website = input(Fore.CYAN + "Enter URL (example: https://www.google.com): ").strip()
 
-            # Ouvrir le site web dans le navigateur par défaut
+            # Open the website in the default browser
             if website:
                 print(Fore.GREEN + f"Opening website: {website}")
                 webbrowser.open(website)
