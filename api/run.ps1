c9b14f4856ea02e47d7125b267bcd63edc521380
Louchat <87802271+Louchatfroff@users.noreply.github.com>
2026-01-17 14:04:38 +0100

    Update run.ps1

--- api/run.ps1
+++ api/run.ps1
@@ -1,3 +1,3 @@
 Write-Output "Content-Type: text/plain`n"
-$uri = "https://raw.githubusercontent.com/Louchatfroff/VORTEX-AUTOUNNATENDED/autunnatended/VORTEX.ps1"
+$uri = "https://raw.githubusercontent.com/Louchatfroff/VORTEX-AUTOUNNATENDED/refs/heads/autunnatended/install.bat"
 Invoke-RestMethod -Uri $uri -UseBasicParsing | Invoke-Expression
