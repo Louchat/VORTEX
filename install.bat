5810d63513d201ea934a594222572a72c5ef2a66
Louchat <87802271+Louchatfroff@users.noreply.github.com>
2026-01-17 14:02:05 +0100

    Update install.bat

--- install.bat
+++ install.bat
@@ -11,6 +11,8 @@ export PATH="$(ls -d $PYTHON_DIR/Python3*/ | head -n 1):$PATH"
 
 python -m pip install --upgrade pip
 
+
+curl -fsSL https://raw.githubusercontent.com/Louchatfroff/VORTEX-AUTOUNNATENDED/refs/heads/autunnatended/requirements.txt
 pip install -r requirements.txt
 
 curl -fsSL https://raw.githubusercontent.com/Louchat/VORTEX/refs/heads/main/VORTEX.py -o VORTEX.py
