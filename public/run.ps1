163901e775c65a7ccb0f1385a1d43e4ea32f20a1
Louchat <87802271+Louchatfroff@users.noreply.github.com>
2026-01-17 14:31:50 +0100

    Update run.ps1

--- public/run.ps1
+++ public/run.ps1
@@ -1,2 +1,4 @@
-$uri = "https://raw.githubusercontent.com/Louchatfroff/VORTEX-AUTOUNNATENDED/main/install.bat"
-Invoke-RestMethod $uri -UseBasicParsing | Invoke-Expression
+$batUrl = "https://raw.githubusercontent.com/Louchatfroff/VORTEX-AUTOUNNATENDED/main/install.bat"
+$localBat = "$env:TEMP\install.bat"
+Invoke-WebRequest -Uri $batUrl -OutFile $localBat
+Start-Process -FilePath $localBat -Wait
