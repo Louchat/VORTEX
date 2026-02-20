6d15476fdaf9b78cd01eaa2cb2c481b851c90655
Louchat <119514120+Louchat@users.noreply.github.com>
2026-02-20 15:06:20 +0100

    Delete public directory

--- public/run.ps1
+++
@@ -1,4 +0,0 @@
-$batUrl = "https://raw.githubusercontent.com/Louchatfroff/VORTEX-AUTOUNNATENDED/main/install.bat"
-$localBat = "$env:TEMP\install.bat"
-Invoke-WebRequest -Uri $batUrl -OutFile $localBat
-Start-Process -FilePath $localBat -Wait
